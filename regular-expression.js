551ae29ec29dcd4d96c5da206e1b9078b6bfca3f
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-06-24 09:38:48 +0300

    reg expression explained

--- regular-expression.js
+++ regular-expression.js
@@ -1,1 +1,10 @@
 // regular expression or regExp is a small programming language that helps to find pattern in data. 
+// used to check if some pattern exists in a different data types. 
+// RegExp parameters : takes two parameters on required search pattern and an optional flag.
+// pattern : can be a text or any form of pattern which some sort of similarity. 
+
+/** flags : are optional parameters in a regular expressions which determine the type of searching 
+ * g : a global flag which means looking for pattern in the whole text 
+ * i : case insensitive flag (it search for both lowercase and uppercase)
+ * m : multiline 
+ */
